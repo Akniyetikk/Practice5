d53517f573b073414d31ec193b256c71fd3a0412
Akniyet <aknietnroe@gmail.com>
2026-03-07 06:30:05 +0500

    Create PythonRegEx.py

---
+++ PythonRegEx.py
@@ -0,0 +1,42 @@
+#Python has a built-in package called re, which can be used to work with Regular Expressions
+import re
+
+ex1
+import re
+#Check if the string starts with "The" and ends with "Spain":
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+if x:
+  print("YES! We have a match!")
+else:
+  print("No match")
+
+#Functions:
+#findall - Returns a list containing all matches
+#search -	Returns a Match object if there is a match anywhere in the string
+#split - Returns a list where the string has been split at each match
+#sub	- Replaces one or many matches with a string
+
+
+#Special Sequences:
+#\A	Returns a match if the specified characters are at the beginning of the string	"\AThe"	
+#\b	Returns a match where the specified characters are at the beginning or at the end of a word
+#(the "r" in the beginning is making sure that the string is being treated as a "raw string")	r"\bain"
+#r"ain\b"	
+
+#\B	Returns a match where the specified characters are present, but NOT at the beginning (or at the end) of a word
+#(the "r" in the beginning is making sure that the string is being treated as a "raw string")	r"\Bain"
+#r"ain\B"	
+
+#\d	Returns a match where the string contains digits (numbers from 0-9)	"\d"	
+#\D	Returns a match where the string DOES NOT contain digits	"\D"	
+#\s	Returns a match where the string contains a white space character	"\s"	
+#\S	Returns a match where the string DOES NOT contain a white space character	"\S"	
+#\w	Returns a match where the string contains any word characters (characters from a to Z, digits from 0-9, and the underscore _ character)	"\w"	
+#\W	Returns a match where the string DOES NOT contain any word characters	"\W"	
+#\Z	Returns a match if the specified characters are at the end of the string	"Spain\Z"
+
+ex18
+
+
+
